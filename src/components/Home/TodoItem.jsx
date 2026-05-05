2724854dbed31eecfbf45b9af119f788ead54bf9
Evil <inc.jackson423@gmail.com>
2026-05-05 19:08:14 +0700

    added delete 'Todo Card' logic

--- src/components/Home/TodoItem.jsx
+++ src/components/Home/TodoItem.jsx
@@ -6,19 +6,28 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Checkbox from "@mui/material/Checkbox";
 
-function TodoItem({todo}) {
+function TodoItem({ todo, handleDeleteTodo, handleDoneTodo }) {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card
+      sx={{
+        minWidth: 275,
+        backgroundColor: todo.isDone ? "lightpink" : undefined,
+      }}
+    >
       <CardContent>
         <Typography gutterBottom>{todo.title}</Typography>
         <Typography variant="body2">{todo.description}</Typography>
       </CardContent>
       <CardActions>
-        <Checkbox checked={todo.isDone} />
+        <Checkbox
+          checked={todo.isDone}
+          onChange={() => handleDoneTodo(todo.id)}
+        />
         <Button size="small">Change</Button>
         <Button
           size="small"
           sx={{ backgroundColor: "crimson", color: "white" }}
+          onClick={() => handleDeleteTodo(todo.id)}
         >
           Delete
         </Button>
