24ee00164572cf5e8fe303d1591247a78ac5608f
evil3v11 <inc.jackson423@gmail.com>
2026-05-05 18:03:12 +0700

    Revert "Accordion"

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { SnackbarProvider } from "notistack";
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <SnackbarProvider>
-    <App />
-  </SnackbarProvider>,
-);
